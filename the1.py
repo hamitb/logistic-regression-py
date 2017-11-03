fd9153727d7a4001032795a2ecb99691aeb85761
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-11-03 22:03:45 +0300

    Read csv into np array

---
+++ the1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pytplot as plt
+
+### Read csv dataset into a numpy array
+data = np.genfromtxt(fname='pima-indians-diabetes.csv', delimiter=',', dtype=float)
+
