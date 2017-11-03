34a637d4bc3fca97d37689d93c0abb1d97f17186
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-11-03 23:39:54 +0300

    Add sigmoid and estimation functions

--- the1.py
+++ the1.py
@@ -1,6 +1,23 @@
 import numpy as np
-import matplotlib.pytplot as plt
+import matplotlib.pyplot as plt
+from math import exp
 
-### Read csv dataset into a numpy array
+### Create np array from csv
 data = np.genfromtxt(fname='pima-indians-diabetes.csv', delimiter=',', dtype=float)
+
+def sigmoid(x, w):
+    expanded_x = np.insert(x, 0, 1.0)
+    w_t_x = 0.0
+
+    for i in range(len(x)):
+        w_t_x += expanded_x[i] * w[i]
+
+    h_w_x = 1.0 / (1.0 + exp(-w_t_x))
+    return h_w_x
+
+def estimate_y_hat(h_w_x):
+    if h_w_x >= 0.5:
+        return 1.0
+    else:
+        return 0.0
 
