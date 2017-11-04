3b9dd1127d55a2baa432f11ac5ed28153973177e
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-11-04 15:43:48 +0300

    Implement logistic regression function

--- the1.py
+++ the1.py
@@ -8,26 +8,31 @@ np.random.seed(499)
 # Create np array from csv
 data = np.genfromtxt(fname='pima-indians-diabetes.csv', delimiter=',', dtype=float)
 
+def train_test_split(X_Y, train_ratio=0.7, first_n_element=0, shuffle=False):
+    X_Y_copy = np.copy(X_Y)
+    if shuffle:
+        np.random.shuffle(X_Y_copy)
+    if first_n_element == 0:
+        first_n_element = X_Y_copy.shape[0] * train_ratio
+    X_train, X_test, Y_train, Y_test = X_Y_copy[:first_n_element, :-1], X_Y_copy[first_n_element:, :-1], X_Y_copy[:first_n_element, -1], X_Y_copy[first_n_element:, -1] 
+    return X_train, X_test, Y_train, Y_test
 def sigmoid(w_t_x):
     h_w_x = 1.0 / (1.0 + exp(-w_t_x))
     return h_w_x
-
 def hyptothesis(x, w):
     w_t_x = 0.0
-    for i in range(len(x)):
-        w_t_x += x[i] * w[i]
+    exp_x = expanded_x(x)
+    for i in range(len(exp_x)):
+        w_t_x += exp_x[i] * w[i]
     return w_t_x
-
 def expanded_x(x):
     exp_x = np.insert(x, 0, 1)
     return exp_x
-
 def estimate_y_hat(h_w_x):
     if h_w_x >= 0.5:
         return 1.0
     else:
         return 0.0
-
 def cost_function(X, Y, w):
     sum_of_errors = 0.0
     m = len(X)
@@ -39,7 +44,6 @@ def cost_function(X, Y, w):
 
     j_w = (-1.0 / m) * sum_of_errors
     return j_w
-
 def cost_function_derivative(X, Y, w, j):
     sum_of_errors = 0
     m = len(X)
@@ -51,7 +55,6 @@ def cost_function_derivative(X, Y, w, j):
         sum_of_errors += (h_w_x - y_i) * x_i_j
     derivative_of_j = (1.0 / m) * sum_of_errors
     return derivative_of_j
-    
 def gradient_descent(X, Y, w, alpha):
     updated_w = np.array([])
     for i in range(len(w)):
@@ -59,3 +62,12 @@ def gradient_descent(X, Y, w, alpha):
         updated_w_i = w[i] - alpha * cf_derivative
         updated_w.append(updated_w_i)
     return updated_w
+def logistic_regression(X, Y, alpha, epochs=10):
+    m = len(Y)
+    w = np.random.randn(len(X[0]) + 1)
+    for i in range(epochs):
+        updated_w = gradient_descent(X, Y, w, alpha)
+        w = updated_w
+        if i % 10 == 0:
+            print 'Current w: ', w
+            print 'Current cost: ', cost_function(X, Y, w)
